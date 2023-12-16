416543fd149aa522898bc0b4a89ec1ff7fee9232
lalitakapadia <lalita.kapadia@gmail.com>
2023-12-16 22:13:55 +0000

    working on asking tasks

--- db/schema.sql
+++ db/schema.sql
@@ -5,24 +5,26 @@ CREATE DATABASE companyDb;
 USE companyDb;
 
 CREATE TABLE department (
-    id INT AUTOINCREMENT,
-    name VARCHAR(30) NOT NULL,
-    PRIMARY KEY (id)
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30) NOT NULL
 );
 
 CREATE TABLE role (
-    id INT PRIMARY KEY AUTOINCREMENT,
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30) NOT NULL,
-    salary DECIMAL(10,2) NOT NULL,
-    department_id INT NOT NULL
-    FOREIGN KEY (id) REFERENCES
-);
+    salary DECIMAL,
+    department_id INT NOT NULL,
+    FOREIGN KEY (department_id) 
+    REFERENCES department(id)
+    );
 
 CREATE TABLE employee (
-    id INT PRIMARY KEY AUTOINCREMENT,
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
-    role_id INT NOT NULL
-    FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE SET NULL,
-    manager_id INT NOT NULL
+    role_id INT,
+    manager_id INT NULL,
+    FOREIGN KEY (role_id) 
+    REFERENCES role(id)
+    ON DELETE SET NULL
 );
